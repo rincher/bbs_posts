4c50d6dc6a5f30de73d69f32bc68727b8f4fd549
hdyoon <skymegod@gmail.com>
2025-08-14 12:58:18 +0900

    /api/*

--- src/main/java/hdyoon/bbs/controller/BbsController.java
+++ src/main/java/hdyoon/bbs/controller/BbsController.java
@@ -3,14 +3,11 @@ package hdyoon.bbs.controller;
 import hdyoon.bbs.dto.BbsPostDto;
 import hdyoon.bbs.service.BbsService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class BbsController {
 
   private final BbsService bbsService;
